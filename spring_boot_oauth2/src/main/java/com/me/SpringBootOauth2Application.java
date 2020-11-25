866e0cdc457140efa344ed7a77e6bfcd9fd2908c
shanth5100 <prashanth.veeraboena@gmail.com>
2020-11-25 11:54:05 +0530

    0.1: project created

---
+++ spring_boot_oauth2/src/main/java/com/me/SpringBootOauth2Application.java
@@ -0,0 +1,13 @@
+package com.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauth2Application.class, args);
+	}
+
+}
